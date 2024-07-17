3048dd20bb32d817713faa017b9b9c396bbf2651
Roshani Devdas naik <roshanni3388@gmail.com>
2024-07-17 20:24:30 +0100

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -9,4 +9,5 @@ def helloall(): # different method name
  name = request.args.get('name')#retrieve GET parameters
  return("Hello {}!".format(name))#Python’s string.format
 if __name__ == "__main__":
- app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
+ if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0')
